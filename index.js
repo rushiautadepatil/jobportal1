549ef201c581c28a576c5f88f0fe491afe43f2bd
rushiautadepatil <autaderushi1997@gmail.com>
2023-12-14 19:48:55 +0530

    added database config and initialzation

--- index.js
+++ index.js
@@ -8,12 +8,17 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.set('view engine','ejs')
 
 mongoose.connect(process.env.MONGODB_URI).then(()=>console.log("connection sucessful")).catch((error)=>console.log(error))
+//health api
+app.get('/health',(req,res)=>{res.json({status:'OK', message:'server is healthy'})})
 
+
+
+//Root route
 app.get('/',(req,res)=>{res.json({
     sucess:true,
     message:'All good'
 })})
 
-app.listen(4000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log('Server running on http 3000')
 })
